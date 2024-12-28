0226dd8dcfcdda7ce5e85f8d9b337fbfb2a471a2
Epaproditus <72703910+epaproditus@users.noreply.github.com>
2024-12-28 09:19:12 -0600

    student filtering api

--- dashboard/src/app/api/matrix/route.ts
+++ dashboard/src/app/api/matrix/route.ts
@@ -116,6 +116,7 @@ export async function POST(request: Request) {
           Campus,
           \`Benchmark PercentScore\` as benchmark_score,
           \`STAAR MA07 Percent Score\` as staar_score,
+          \`Local Id\` as id, // Selecting Local Id as the student ID
           \`Benchmark Teacher\` as Teacher
         FROM (
           SELECT * FROM data
